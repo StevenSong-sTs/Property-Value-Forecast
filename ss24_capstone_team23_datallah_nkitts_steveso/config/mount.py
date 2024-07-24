0e2bac8fc019eba047395a2cac8f04572db32319
datallah@umich.edu <datallah@umich.edu>
2024-07-23 21:15:48 -0600

    drive can only access one gh nb at a time...removing

--- ss24_capstone_team23_datallah_nkitts_steveso/config/mount.py
+++
@@ -1,6 +0,0 @@
-# ensure that capstone shared folder is in "MyDrive"
-from google.colab import drive
-drive.mount('/content/drive')
-
-raw_path = 'drive/MyDrive/MADS Capstone Team 23/Data/raw/'
-processed_path = 'drive/MyDrive/MADS Capstone Team 23/Data/processed/'
